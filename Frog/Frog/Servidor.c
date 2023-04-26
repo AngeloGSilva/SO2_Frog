73033dbc8eb5c11f89eb651ec39ddf1a13cc8eda
AngeloGSilva <47340682+AngeloGSilva@users.noreply.github.com>
2023-04-26 19:48:10 +0100

    ThreadRoads not Tested

--- Frog/Frog/Servidor.c
+++ Frog/Frog/Servidor.c
@@ -12,6 +12,62 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define MAX_ROADS_THREADS 50
+
+typedef struct {
+	int speed;
+	BOOLEAN direction;
+	pGameData Game;
+	HANDLE hSemRoads, hMutex;
+	int id;
+} TRoads, * pTRoads;
+
+
+DWORD WINAPI ThreadRoads(LPVOID lpParam)
+{
+	pTRoads data = (pTRoads)lpParam;
+	_tprintf(TEXT("n de carros%d\n"), data->Game->numCars);
+	while (1)
+	{
+		WaitForSingleObject(data->hMutex, INFINITE);
+		_tprintf(TEXT("thread %d comecou\n"),data->id);
+		//movimento carros direita esquerda
+		for (int i = 0; i < data->Game->numCars; i++)
+		{
+			int x = data->Game->car_pos[i][0];
+			if (x == data->id)
+			{
+				_tprintf(TEXT("carro: %d da thread %d\n"), x, data->id);
+				int y = data->Game->car_pos[i][1];
+				data->Game->map[x][y] = ROAD_ELEMENT;
+				if (data->Game->car_pos[i][1] - 1 == 0)
+					data->Game->car_pos[i][1] = MAX_COLS - 2;
+				else
+					data->Game->car_pos[i][1]--;
+				data->Game->map[x][y] = CAR_ELEMENT;
+				_tprintf(TEXT("Thread da estrada %d: fez o carro andar\n"), data->id);
+			}
+		}
+
+		ReleaseMutex(data->hMutex);
+		_tprintf(TEXT("thread %d acabou\n"), data->id);
+		Sleep(((rand() % 8) + 1)*2000);
+
+	}
+	
+	////atualizar mapa
+	//for (int i = 0; i < data->Game.numCars; i++)
+	//{
+	//	int x = data->Game.car_pos[i][0];
+	//	int y = data->Game.car_pos[i][1];
+	//	_tprintf(TEXT("x:%d\n"), x);
+	//	_tprintf(TEXT("y:%d\n"), y);
+	//	data->Game.map[x][y] = CAR_ELEMENT;
+	//}
+
+	return 0;
+}
+
 
 DWORD WINAPI ThreadBufferCircular(LPVOID lpParam)
 {
@@ -161,6 +217,8 @@ int _tmain(int argc, TCHAR* argv[]) {
 	data.numCars = 0;
 	_tprintf(TEXT("NumRoads %d\n"), data.numRoads);
 
+
+
 	//Gerar carros
 	for (int i = 0; i < data.numRoads; i++) 
 	{
@@ -186,6 +244,42 @@ int _tmain(int argc, TCHAR* argv[]) {
 		}
 	}
 
+
+	HANDLE mutex_ROADS;
+
+	// matriz de handles das threads
+	HANDLE RoadThreads[MAX_ROADS_THREADS];
+
+	TRoads RoadsData[MAX_ROADS_THREADS];
+
+
+
+	for (int i = 0; i < data.numRoads; i++) 
+	{
+		_tprintf(TEXT("[~DEBUG] Thread estrada %d criada\n"),i);
+		RoadsData[i].hMutex = CreateMutex(NULL, FALSE, TEXT("MUTEX_ROADS"));
+		RoadsData[i].Game = &data;
+		RoadsData[i].id = i + 2; //o numero do id é a estrada q elas estao encarregues
+		RoadsData[i].speed = 0;
+		RoadsData[i].direction = 1;
+		RoadThreads[i] = CreateThread(
+			NULL,    // Thread attributes
+			0,       // Stack size (0 = use default)
+			ThreadRoads, // Thread start address
+			&RoadsData[i],    // Parameter to pass to the thread
+			CREATE_SUSPENDED,       // Creation flags
+			NULL);   // Thread id   // returns the thread identifier 
+	}
+
+	for (int i = 0; i < data.numRoads; i++)
+	{
+		_tprintf(TEXT("[~DEBUG] Thread estrada %d iniciada\n"), i);
+		ResumeThread(RoadThreads[i]);
+	}
+
+	// FAZER aguarda / controla as threads 
+	//       manda as threads parar
+	WaitForMultipleObjects(data.numRoads, RoadThreads, TRUE, INFINITE);
 
 	////Gerar carros
 	//for (int i = 2; i < MAX_ROWS + 2; i++)
@@ -227,76 +321,76 @@ int _tmain(int argc, TCHAR* argv[]) {
 		return 0;
 	}
 
-	while (1)
-	{
-		//movimento carros esquerda direita
-		/*for (int i = 0; i < data.numCars; i++)
-		{
-			int x = data.car_pos[i][0];
-			int y = data.car_pos[i][1];
-			data.map[x][y] = ROAD_ELEMENT;
-			if (data.car_pos[i][1] + 2 == MAX_COLS)
-				data.car_pos[i][1] = 1;
-			else  
-				data.car_pos[i][1]++;
-		}*/
+	//while (1)
+	//{
+	//	//movimento carros esquerda direita
+	//	/*for (int i = 0; i < data.numCars; i++)
+	//	{
+	//		int x = data.car_pos[i][0];
+	//		int y = data.car_pos[i][1];
+	//		data.map[x][y] = ROAD_ELEMENT;
+	//		if (data.car_pos[i][1] + 2 == MAX_COLS)
+	//			data.car_pos[i][1] = 1;
+	//		else  
+	//			data.car_pos[i][1]++;
+	//	}*/
 
-		//movimento carros direita esquerda
-		for (int i = 0; i < data.numCars; i++)
-		{
-			int x = data.car_pos[i][0];
-			int y = data.car_pos[i][1];
-			data.map[x][y] = ROAD_ELEMENT;
-			if (data.car_pos[i][1] - 1 == 0)
-				data.car_pos[i][1] = MAX_COLS - 2;
-			else
-				data.car_pos[i][1]--;
-		}
-		//atualizar mapa
-		for (int i = 0; i < data.numCars; i++)
-		{
-			int x = data.car_pos[i][0];
-			int y = data.car_pos[i][1];
-			_tprintf(TEXT("x:%d\n"),x);
-			_tprintf(TEXT("y:%d\n"),y);
-			data.map[x][y] = CAR_ELEMENT;
-		}
+	//	//movimento carros direita esquerda
+	//	for (int i = 0; i < data.numCars; i++)
+	//	{
+	//		int x = data.car_pos[i][0];
+	//		int y = data.car_pos[i][1];
+	//		data.map[x][y] = ROAD_ELEMENT;
+	//		if (data.car_pos[i][1] - 1 == 0)
+	//			data.car_pos[i][1] = MAX_COLS - 2;
+	//		else
+	//			data.car_pos[i][1]--;
+	//	}
+	//	//atualizar mapa
+	//	for (int i = 0; i < data.numCars; i++)
+	//	{
+	//		int x = data.car_pos[i][0];
+	//		int y = data.car_pos[i][1];
+	//		_tprintf(TEXT("x:%d\n"),x);
+	//		_tprintf(TEXT("y:%d\n"),y);
+	//		data.map[x][y] = CAR_ELEMENT;
+	//	}
 
-		//for (int i = 0; i < MAX_ROWS + 4; i++)
-		//{
-		//	int next_col = 0;
-		//	for (int j = 0; j < MAX_COLS; j++)
-		//	{
-		//		if (data.map[i][j] == CAR_ELEMENT) {
-		//			//next_col = (j + 1) % MAX_COLS;
-		//			if (j + 1 != MAX_COLS - 1) {
-		//				data.map[i][j + 1] = CAR_ELEMENT;
-		//				data.map[i][j] = ROAD_ELEMENT;
-		//			}
-		//			else if(j + 1 == MAX_COLS - 1){
-		//				data.map[i][1] = CAR_ELEMENT;
-		//				data.map[i][j] = ROAD_ELEMENT;
-		//			}
-		//		}
-		//	}
-		//	//adicionar ao array de posições de carros e gerar as posições no mapa de uma vez
-		//}
+	//	//for (int i = 0; i < MAX_ROWS + 4; i++)
+	//	//{
+	//	//	int next_col = 0;
+	//	//	for (int j = 0; j < MAX_COLS; j++)
+	//	//	{
+	//	//		if (data.map[i][j] == CAR_ELEMENT) {
+	//	//			//next_col = (j + 1) % MAX_COLS;
+	//	//			if (j + 1 != MAX_COLS - 1) {
+	//	//				data.map[i][j + 1] = CAR_ELEMENT;
+	//	//				data.map[i][j] = ROAD_ELEMENT;
+	//	//			}
+	//	//			else if(j + 1 == MAX_COLS - 1){
+	//	//				data.map[i][1] = CAR_ELEMENT;
+	//	//				data.map[i][j] = ROAD_ELEMENT;
+	//	//			}
+	//	//		}
+	//	//	}
+	//	//	//adicionar ao array de posições de carros e gerar as posições no mapa de uma vez
+	//	//}
 
-		Sleep(data.carSpeed);
-		WaitForSingleObject(data.Serv_HMutex, INFINITE);
+	//	Sleep(data.carSpeed);
+	//	WaitForSingleObject(data.Serv_HMutex, INFINITE);
 
-		ZeroMemory(pBuf, sizeof(GameData));
-		CopyMemory(pBuf, &data, sizeof(GameData));
+	//	ZeroMemory(pBuf, sizeof(GameData));
+	//	CopyMemory(pBuf, &data, sizeof(GameData));
 
-		//libertat o mutex
-		ReleaseMutex(data.Serv_HMutex);
+	//	//libertat o mutex
+	//	ReleaseMutex(data.Serv_HMutex);
 
-		//Criamos evento para que as threads ja consiga ler
-		SetEvent(data.Serv_HEvent);
+	//	//Criamos evento para que as threads ja consiga ler
+	//	SetEvent(data.Serv_HEvent);
 
-		Sleep(500);
-		ResetEvent(data.Serv_HEvent);
-	}
+	//	Sleep(500);
+	//	ResetEvent(data.Serv_HEvent);
+	//}
 	CloseHandle(hThreads);
 	//ReleaseSemaphore(hSem, 1, NULL);
 	//UnmapViewOfFile(pBuf);
