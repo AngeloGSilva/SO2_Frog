871d74f049c85a4367cc50646cd57e2f0e75354e
AngeloGSilva <47340682+AngeloGSilva@users.noreply.github.com>
2023-05-25 21:44:43 +0100

    mais alteracoes e ideias

--- Frog/Frog/Servidor.c
+++ Frog/Frog/Servidor.c
@@ -13,6 +13,62 @@
 #include <string.h>
 
 
+//Pipe
+#define PIPE_NAME TEXT("\\\\.\\pipe\\SERVER_CLIENTE")
+#define N 10
+
+typedef struct {
+	HANDLE hPipe[N];
+	HANDLE hMutex; //para controlar o numClientes
+	int numClientes;
+	int terminar;
+}ThreadDadosPIPE;
+
+//envia mensagem para todos os leitores que estão disponiveis
+DWORD WINAPI ThreadMensagens(LPVOID param) {
+	ThreadDadosPIPE* dados = (ThreadDadosPIPE*)param;
+	TCHAR buf[256];
+	DWORD n;
+	int i;
+	BOOL ret;
+
+	//aqui , o servidor já recebeu um cliente
+	do {
+		//le as mensagens enviadas pelo servidor
+		ret = ReadFile(dados.hPipe, buf, sizeof(buf), &n, NULL);
+
+		//termina corretamente a string
+		buf[n / sizeof(TCHAR)] = '\0';
+
+		if (!ret || !n) {
+			_tprintf(TEXT("[LEITOR] %d %d... (ReadFile)\n"), ret, n);
+			break;
+		}
+
+		_tprintf(TEXT("[LEITOR] Recebi %d bytes: '%s'... (ReadFile)\n"), n, buf);
+
+
+	} while (_tcscmp(buf, TEXT("fim")));
+
+	dados->terminar = 1;
+
+	return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 DWORD WINAPI ThreadRoads(LPVOID lpParam)
 {
 	pTRoads data = (pTRoads)lpParam;
@@ -598,6 +654,86 @@ int _tmain(int argc, TCHAR* argv[]) {
 		return 1;
 	}
 
+
+
+	//named PIPES
+
+	HANDLE hPipe, hThread;
+	ThreadDadosPIPE dados;
+	dados.numClientes = 0;
+	dados.terminar = 0;
+	dados.hMutex = CreateMutex(NULL, FALSE, NULL); //Criação do mutex
+
+	if (dados.hMutex == NULL) {
+		_tprintf(TEXT("[Erro] ao criar mutex!\n"));
+		return -1;
+	}
+
+	//em vez de aqui tem de ser uma thread por cliente a ser criada dentro do while penso eu e so passa o handle do pipe desse cliente e nao todos
+	//criacao da thread
+	hThread = CreateThread(NULL, 0, ThreadMensagens, &dados, 0, NULL);
+	if (hThread == NULL) {
+		_tprintf(TEXT("[Erro] ao criar thread!\n"));
+		return -1;
+	}
+
+	_tprintf(TEXT("[Leitor] Criar uma cópia do pipe '%s' ... (CreateNamedPipe)\n"), PIPE_NAME);
+
+
+
+	while (!dados.terminar) {
+
+		// PIPE_ACCESS_OUTBOUND -> o ESCRITOR escreve e o LEITOR recebe
+		//1º nome do named pipe, 2ºconfiguração da abertura, 3ºconfigurações do pipe,4º numero maximo de instancias,5º e 6ºtamanho do buffer de entrada e de saida
+		//7º timeout, 8º security atributes
+		hPipe = CreateNamedPipe(PIPE_NAME, PIPE_ACCESS_OUTBOUND, PIPE_WAIT | PIPE_TYPE_MESSAGE | PIPE_READMODE_MESSAGE, N, 256 * sizeof(TCHAR), 256 * sizeof(TCHAR), 1000, NULL);
+
+		if (hPipe == INVALID_HANDLE_VALUE) {
+			_tprintf(TEXT("[ERRO] Criar Named Pipe! (CreateNamedPipe)"));
+			exit(-1);
+		}
+
+
+		_tprintf(TEXT("[LEITOR] Esperar ligação de um leitor... (ConnectNamedPipe)\n"));
+
+		//o servidor espera até ter um cliente conectado a esta instância
+		//bloqueia aqui
+		if (!ConnectNamedPipe(hPipe, NULL)) {
+			_tprintf(TEXT("[ERRO] Ligação ao leitor! (ConnectNamedPipe\n"));
+			exit(-1);
+		}
+
+		//bloqueamos no mutex
+		WaitForSingleObject(dados.hMutex, INFINITE);
+
+		//preenchemos o array
+		dados.hPipe[dados.numClientes] = hPipe;
+		dados.numClientes++;
+
+		//libertamos o mutex
+		ReleaseMutex(dados.hMutex);
+
+	}
+
+	//esperar que a thread termine
+	WaitForSingleObject(hThread, INFINITE);
+
+	//desligamos todos os clientes que se ligaram
+	for (int i = 0; i < dados.numClientes; i++) {
+		_tprintf(TEXT("[Leitor] Desligar o pipe (DisconnectNamedPipe)\n"));
+
+		//desliga a instancia ao named pipe
+		if (!DisconnectNamedPipe(dados.hPipe[i])) {
+			_tprintf(TEXT("[ERRO] Desligar o pipe! (DisconnectNamedPipe)"));
+			exit(-1);
+		}
+	}
+
+
+
+
+
+	//provavelmente nao preciso mais disto
 	while (terminar == 0)
 	{
 
