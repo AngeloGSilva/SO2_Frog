44a80a3078f0fe32ada8a58a7a05cad5e5701dbb
MRodrigo1 <76001389+MRodrigo1@users.noreply.github.com>
2023-06-03 16:23:55 +0100

    Revert "mais alteracoes e ideias"
    This reverts commit 871d74f049c85a4367cc50646cd57e2f0e75354e.

--- Frog/Cliente/Cliente.c
+++ Frog/Cliente/Cliente.c
@@ -37,47 +37,14 @@ int _tmain(int argc, TCHAR* argv[]) {
 	_setmode(_fileno(stdin), _O_WTEXT);
 	_setmode(_fileno(stdout), _O_WTEXT);
 #endif
-	HANDLE hPipe, hEventPipe;
-	TCHAR buf[256];
-	DWORD n;
-	int i;
 
-	_tprintf(TEXT("[Escritor] Esperar pelo pipe '%s' (WaitNamedPipe)\n"), PIPE_NAME);
-	//bloqueia aqui
-	if (!WaitNamedPipe(PIPE_NAME, NMPWAIT_WAIT_FOREVER)) {
-		_tprintf(TEXT("[ERRO] Ligar ao pipe '%s'! (WaitNamedPipe)\n"), PIPE_NAME);
-		exit(-1);
-	}
-	_tprintf(TEXT("[Escritor] Ligação ao pipe do leitor... (CreateFile)\n"));
-
-	hPipe = CreateNamedPipe(PIPE_NAME,GENERIC_READ,0,NULL,OPEN_EXISTING,FILE_ATTRIBUTE_NORMAL,NULL);
-	if (hPipe == NULL) {
-		_tprintf(TEXT("[ERRO] Ligar ao pipe '%s'! (CreateFile)\n"), PIPE_NAME);
-		exit(-1);
-	}
-	_tprintf(TEXT("[Escritor] Liguei-me...\n"));
-	HANDLE mutex = CreateMutex(NULL, FALSE, NULL);
-	//aqui , o servidor já recebeu um cliente
-	do {
-		//vai buscar informação à consola
-		_tprintf(TEXT("[ESCRITOR] Frase: "));
-		_fgetts(buf, 256, stdin);
-		buf[_tcslen(buf) - 1] = '\0';
-
-		//bloqueamos aqui porque é uma regiao critica
-		WaitForSingleObject(mutex, INFINITE);
-
-			if (!WriteFile(hPipe, buf, _tcslen(buf) * sizeof(TCHAR), &n, NULL)) {
-				_tprintf(TEXT("[ERRO] Escrever no pipe! (WriteFile)\n"));
-				exit(-1);
-			}
-
-			_tprintf(TEXT("[ESCRITOR] Enviei %d bytes ao leitor [%d]... (WriteFile)\n"), n, i);
-		//libertamos o mutex
-		ReleaseMutex(mutex);
-
-
-	} while (_tcscmp(buf, TEXT("fim")));
+	HANDLE hPipe = CreateNamedPipe(PIPE_NAME, PIPE_ACCESS_OUTBOUND | FILE_FLAG_OVERLAPPED,PIPE_WAIT|PIPE_TYPE_BYTE|PIPE_READMODE_BYTE,
+		N,
+		(nao sei),
+		(nao sei),
+		(nao sei),
+		NULL
+		);
 
 
 	return 0;
