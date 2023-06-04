6a49676c69561dab9715bcc3b9c4247473226adf
MRodrigo1 <76001389+MRodrigo1@users.noreply.github.com>
2023-06-04 15:40:19 +0100

    Initial car Map

--- Frog/Cliente/Cliente.c
+++ Frog/Cliente/Cliente.c
@@ -43,6 +43,23 @@ typedef struct {
 	int terminar;
 };
 
+
+
+
+/* ===================================================== */
+/* Programa base (esqueleto) para aplicações Windows     */
+/* ===================================================== */
+// Cria uma janela de nome "Janela Principal" e pinta fundo de branco
+// Modelo para programas Windows:
+//  Composto por 2 funções: 
+//	WinMain()     = Ponto de entrada dos programas windows
+//			1) Define, cria e mostra a janela
+//			2) Loop de recepção de mensagens provenientes do Windows
+//     TrataEventos()= Processamentos da janela (pode ter outro nome)
+//			1) É chamada pelo Windows (callback) 
+//			2) Executa código em função da mensagem recebida
+
+// Função de callback que será chamada pelo Windows sempre que acontece alguma coisa
 LRESULT CALLBACK TrataEventos(HWND, UINT, WPARAM, LPARAM);
 
 // Nome da classe da janela (para programas de uma só janela, normalmente este nome é 
@@ -65,36 +82,86 @@ TCHAR szProgName[] = TEXT("Base");
 //   nCmdShow:  Parâmetro que especifica o modo de exibição da janela (usado em  
 //        	   ShowWindow()
 
+//BITMAP
+// uma vez que temos de usar estas vars tanto na main como na funcao de tratamento de eventos
+// nao ha uma maneira de fugir ao uso de vars globais, dai estarem aqui
+HBITMAP hBmp; // handle para o bitmap
+HDC bmpDC; // hdc do bitmap
+BITMAP bmp; // informação sobre o bitmap
+int xBitmap; // posicao onde o bitmap vai ser desenhado
+int yBitmap;
+
+int limDir; // limite direito
+HWND hWndGlobal; // handle para a janela
+HANDLE hMutex;
+
+HDC memDC = NULL; // copia do device context que esta em memoria, tem de ser inicializado a null
+HBITMAP hBitmapDB; // copia as caracteristicas da janela original para a janela que vai estar em memoria
+GameData* AllGameData ;
+
+
+// Mexe na posição x da imagem de forma a que a imagem se vá movendo
+DWORD WINAPI RefreshMap(LPVOID lParam) {
+	int dir = 1; // 1 para a direita, -1 para a esquerda
+	int salto = 2; // quantidade de pixeis que a imagem salta de cada vez
+
+	while (1) {
+		// Aguarda que o mutex esteja livre
+		WaitForSingleObject(hMutex, INFINITE);
+
+		// movimentação
+		xBitmap = xBitmap + (dir * salto);
+
+		//fronteira À esquerda
+		if (xBitmap <= 0) {
+			xBitmap = 0;
+			dir = 1;
+		}
+		// limite direito
+		else if (xBitmap >= limDir) {
+			xBitmap = limDir;
+			dir = -1;
+		}
+		//liberta mutex
+		ReleaseMutex(hMutex);
+
+		// dizemos ao sistema que a posição da imagem mudou e temos entao de fazer o refresh da janela
+		InvalidateRect(hWndGlobal, NULL, FALSE);
+		Sleep(1);
+	}
+	return 0;
+
+}
+
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR lpCmdLine, int nCmdShow) {
 	HWND hWnd;		// hWnd é o handler da janela, gerado mais abaixo por CreateWindow()
 	MSG lpMsg;		// MSG é uma estrutura definida no Windows para as mensagens
-	WNDCLASSEX wcApp;	// WNDCLASSEX é uma estrutura cujos membros servem para 
+	WNDCLASSEX wcApp;	// WNDCLASSEX é uma estrutura cujos membros servem para
 	// definir as características da classe da janela
-	//DADOS QUE VAO SER USADOS
+
 	int terminar = 0; // para acabar com tudo
-		TCHAR buf[256];
-		HANDLE hPipe;
-		int i = 0;
-		BOOL ret;
-		DWORD n;
-	    GameData* AllGameData = (GameData*)malloc(sizeof(GameData));
+	TCHAR buf[256];
+	HANDLE hPipe;
+	int i = 0;
+	BOOL ret;
+	DWORD n;
 
-    if (!WaitNamedPipe(PIPE_NAME, NMPWAIT_WAIT_FOREVER)) {
-    }
-    hPipe = CreateFile(PIPE_NAME, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
-    if (hPipe == NULL) {
-    }
-
-    ret = ReadFile(hPipe, AllGameData, sizeof(GameData), &n, NULL);
-    CloseHandle(hPipe);
+	if (!WaitNamedPipe(PIPE_NAME, NMPWAIT_WAIT_FOREVER)) {
+	}
+	hPipe = CreateFile(PIPE_NAME, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+	if (hPipe == NULL) {
+	}
+	AllGameData = (GameData*)malloc(sizeof(GameData));
+	ret = ReadFile(hPipe, AllGameData, sizeof(GameData), &n, NULL);
+	
 
 // ============================================================================
-// 1. Definição das características da janela "wcApp" 
+// 1. Definição das características da janela "wcApp"
 //    (Valores dos elementos da estrutura "wcApp" do tipo WNDCLASSEX)
 // ============================================================================
 	wcApp.cbSize = sizeof(WNDCLASSEX);      // Tamanho da estrutura WNDCLASSEX
-	wcApp.hInstance = hInst;		         // Instância da janela actualmente exibida 
-	// ("hInst" é parâmetro de WinMain e vem 
+	wcApp.hInstance = hInst;		         // Instância da janela actualmente exibida
+	// ("hInst" é parâmetro de WinMain e vem
 		  // inicializada daí)
 	wcApp.lpszClassName = szProgName;       // Nome da janela (neste caso = nome do programa)
 	wcApp.lpfnWndProc = TrataEventos;       // Endereço da função de processamento da janela
@@ -109,20 +176,21 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR lpCmdLine, int nC
 	wcApp.hIconSm = LoadIcon(NULL, IDI_INFORMATION); // "hIconSm" = handler do ícon pequeno
 	// "NULL" = Icon definido no Windows
 	// "IDI_INF..." Ícon de informação
-	wcApp.hCursor = LoadCursor(NULL, IDC_ARROW);	// "hCursor" = handler do cursor (rato) 
+	wcApp.hCursor = LoadCursor(NULL, IDC_ARROW);	// "hCursor" = handler do cursor (rato)
 	// "NULL" = Forma definida no Windows
-	// "IDC_ARROW" Aspecto "seta" 
+	// "IDC_ARROW" Aspecto "seta"
 	wcApp.lpszMenuName = NULL;			// Classe do menu que a janela pode ter
 	// (NULL = não tem menu)
 	wcApp.cbClsExtra = 0;				// Livre, para uso particular
 	wcApp.cbWndExtra = 0;				// Livre, para uso particular
-	wcApp.hbrBackground = CreateSolidBrush(RGB(255, 100, 100));
-	// "hbrBackground" = handler para "brush" de pintura do fundo da janela. Devolvido por
-	// "GetStockObject".Neste caso o fundo será branco
+	wcApp.hbrBackground = CreateSolidBrush(RGB(125, 125, 125));
+	//(HBRUSH)GetStockObject(WHITE_BRUSH);
+// "hbrBackground" = handler para "brush" de pintura do fundo da janela. Devolvido por
+// "GetStockObject".Neste caso o fundo será branco
 
-	// ============================================================================
-	// 2. Registar a classe "wcApp" no Windows
-	// ============================================================================
+// ============================================================================
+// 2. Registar a classe "wcApp" no Windows
+// ============================================================================
 	if (!RegisterClassEx(&wcApp))
 		return(0);
 
@@ -131,41 +199,81 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR lpCmdLine, int nC
 	// ============================================================================
 	hWnd = CreateWindow(
 		szProgName,			// Nome da janela (programa) definido acima
-		TEXT("Exemplo de Janela Principal em C"),// Texto que figura na barra do título
+		TEXT("Janelaaaa bruh"),// Texto que figura na barra do título
 		WS_OVERLAPPEDWINDOW,	// Estilo da janela (WS_OVERLAPPED= normal)
 		CW_USEDEFAULT,		// Posição x pixels (default=à direita da última)
 		CW_USEDEFAULT,		// Posição y pixels (default=abaixo da última)
 		CW_USEDEFAULT,		// Largura da janela (em pixels)
 		CW_USEDEFAULT,		// Altura da janela (em pixels)
 		(HWND)HWND_DESKTOP,	// handle da janela pai (se se criar uma a partir de
-		// outra) ou HWND_DESKTOP se a janela for a primeira, 
+		// outra) ou HWND_DESKTOP se a janela for a primeira,
 		// criada a partir do "desktop"
 		(HMENU)NULL,			// handle do menu da janela (se tiver menu)
-		(HINSTANCE)hInst,		// handle da instância do programa actual ("hInst" é 
+		(HINSTANCE)hInst,		// handle da instância do programa actual ("hInst" é
 		// passado num dos parâmetros de WinMain()
 		0);				// Não há parâmetros adicionais para a janela
+
+	HDC hdc; // representa a propria janela
+	RECT rect;
+
+	// carregar o bitmap
+	hBmp = (HBITMAP)LoadImage(NULL, TEXT("car.bmp"), IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
+	GetObject(hBmp, sizeof(bmp), &bmp); // vai buscar info sobre o handle do bitmap
+
+	hdc = GetDC(hWnd);
+	// criamos copia do device context e colocar em memoria
+	bmpDC = CreateCompatibleDC(hdc);
+	// aplicamos o bitmap ao device context
+	SelectObject(bmpDC, hBmp);
+
+	ReleaseDC(hWnd, hdc);
+
+
+	// EXEMPLO
+	// 800 px de largura, imagem 40px de largura
+	// ponto central da janela 400 px(800/2)
+	// imagem centrada, começar no 380px e acabar no 420 px
+	// (800/2) - (40/2) = 400 - 20 = 380px
+
+	// definir as posicoes inicias da imagem
+	GetClientRect(hWnd, &rect);
+	xBitmap = (rect.right / 2) - (bmp.bmWidth / 2);
+	yBitmap = (rect.bottom / 2) - (bmp.bmHeight / 2);
+
+	// limite direito é a largura da janela - largura da imagem
+	limDir = rect.right - bmp.bmWidth;
+	hWndGlobal = hWnd;
+
+	// Cria mutex
+	hMutex = CreateMutex(NULL, FALSE, NULL);
+
+	// Cria a thread de movimentação
+	CreateThread(NULL, 0, RefreshMap, NULL, 0, NULL);
+
+
 	// ============================================================================
 	// 4. Mostrar a janela
 	// ============================================================================
-	ShowWindow(hWnd, nCmdShow);	// "hWnd"= handler da janela, devolvido por 
-	// "CreateWindow"; "nCmdShow"= modo de exibição (p.e. 
+	ShowWindow(hWnd, nCmdShow);	// "hWnd"= handler da janela, devolvido por
+	// "CreateWindow"; "nCmdShow"= modo de exibição (p.e.
 	// normal/modal); é passado como parâmetro de WinMain()
-	UpdateWindow(hWnd);		// Refrescar a janela (Windows envia à janela uma 
-	// mensagem para pintar, mostrar dados, (refrescar)… 
+	UpdateWindow(hWnd);		// Refrescar a janela (Windows envia à janela uma
+	// mensagem para pintar, mostrar dados, (refrescar)
+
 // ============================================================================
 // 5. Loop de Mensagens
 // ============================================================================
 // O Windows envia mensagens às janelas (programas). Estas mensagens ficam numa fila de
-// espera até que GetMessage(...) possa ler "a mensagem seguinte"	
+// espera até que GetMessage(...) possa ler "a mensagem seguinte"
 // Parâmetros de "getMessage":
-// 1)"&lpMsg"=Endereço de uma estrutura do tipo MSG ("MSG lpMsg" ja foi declarada no  
+// 1)"&lpMsg"=Endereço de uma estrutura do tipo MSG ("MSG lpMsg" ja foi declarada no
 //   início de WinMain()):
 //			HWND hwnd		handler da janela a que se destina a mensagem
 //			UINT message		Identificador da mensagem
 //			WPARAM wParam		Parâmetro, p.e. código da tecla premida
 //			LPARAM lParam		Parâmetro, p.e. se ALT também estava premida
 //			DWORD time		Hora a que a mensagem foi enviada pelo Windows
-//			POINT pt		Localização do mouse (x, y) 
+//			POINT pt		Localização do mouse (x, y)
 // 2)handle da window para a qual se pretendem receber mensagens (=NULL se se pretendem
 //   receber as mensagens para todas as
 // janelas pertencentes à thread actual)
@@ -173,13 +281,13 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR lpCmdLine, int nC
 // 4)Código limite superior das mensagens que se pretendem receber
 
 // NOTA: GetMessage() devolve 0 quando for recebida a mensagem de fecho da janela,
-// 	  terminando então o loop de recepção de mensagens, e o programa 
+// 	  terminando então o loop de recepção de mensagens, e o programa
 
 	while (GetMessage(&lpMsg, NULL, 0, 0)) {
-		TranslateMessage(&lpMsg);	// Pré-processamento da mensagem (p.e. obter código 
+		TranslateMessage(&lpMsg);	// Pré-processamento da mensagem (p.e. obter código
 		// ASCII da tecla premida)
 		DispatchMessage(&lpMsg);	// Enviar a mensagem traduzida de volta ao Windows, que
-		// aguarda até que a possa reenviar à função de 
+		// aguarda até que a possa reenviar à função de
 		// tratamento da janela, CALLBACK TrataEventos (abaixo)
 	}
 
@@ -207,49 +315,110 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR lpCmdLine, int nC
 //
 // A função EndProc é sempre do tipo "switch..." com "cases" que descriminam a mensagem
 // recebida e a tratar.
-// Estas mensagens são identificadas por constantes (p.e. 
+// Estas mensagens são identificadas por constantes (p.e.
 // WM_DESTROY, WM_CHAR, WM_KEYDOWN, WM_PAINT...) definidas em windows.h
 // ============================================================================
 
+
+
 LRESULT CALLBACK TrataEventos(HWND hWnd, UINT messg, WPARAM wParam, LPARAM lParam) {
+	//handle para o device context
 	HDC hdc;
+	PAINTSTRUCT ps;
 	RECT rect;
-	int xPos, yPos;
-	static TCHAR c = '?';
-	static int contador = 1;
+	srand((unsigned)time(NULL));
 
-	switch (messg) {
-	case WM_LBUTTONDOWN:
-		xPos = GET_X_LPARAM(lParam);
-		yPos = GET_Y_LPARAM(lParam);
-		hdc = GetDC(hWnd);
+
+	switch (messg)
+	{
+		// evento que é disparado sempre que o sistema pede um refrescamento da janela
+	case WM_PAINT:
+		// Inicio da pintura da janela, que substitui o GetDC
+		hdc = BeginPaint(hWnd, &ps);
 		GetClientRect(hWnd, &rect);
-		SetTextColor(hdc, RGB(255, 255, 255));
-		SetBkMode(hdc, TRANSPARENT);
-		rect.left = xPos;
-		rect.top = yPos;
-		for (int i = 0; i < contador; i++)
-		{
-			DrawText(hdc, &c, 1, &rect, DT_SINGLELINE | DT_NOCLIP);
-			rect.left += 8;
-		}
-		ReleaseDC(hWnd, hdc);
-		break;
-	case WM_RBUTTONDOWN:
-		contador++;
-		break;
-	case WM_CHAR:
-		c = (TCHAR)wParam;
-		if (c == VK_SPACE)
-			contador = 1;
-		break;
-	case WM_CLOSE:
 
-		if (MessageBox(hWnd, TEXT("Sure you want to get out?"), TEXT("Leaving?"), MB_OKCANCEL) == 1)
-			DestroyWindow(hWnd);
+		// se a copia estiver a NULL, significa que é a 1ª vez que estamos a passar no WM_PAINT e estamos a trabalhar com a copia em memoria
+		if (memDC == NULL) {
+			PAINTSTRUCT ps;
+
+			// Set the brush color and style
+			HBRUSH hBrush = CreateSolidBrush(RGB(0, 0, 255));
+			SelectObject(hdc, hBrush);
+
+			// Draw the map
+			RECT rect;
+			rect.left = 100;
+			rect.top = 100;
+			rect.right = rect.left + 500;
+			rect.bottom = rect.top + 500;
+			FillRect(hdc, &rect, hBrush);
+			// Draw the road
+			RECT road;
+			for (int i = 0; i < AllGameData->numRoads; i++) {
+				COLORREF myColorRef = RGB(
+					(BYTE)(rand() % 255), // red component of color
+					(BYTE)(rand() % 255), // green component of color
+					(BYTE)(rand() % 255) // blue component of color
+				);
+				HBRUSH hBrush = CreateSolidBrush(myColorRef);
+				SelectObject(hdc, hBrush);
+				road.left = 100;
+				road.top = 150 +(i * 50);
+				road.right = road.left + 500;
+				road.bottom = road.top + 50;
+				FillRect(hdc, &road, hBrush);
+			}
+			// Cleanup
+			DeleteObject(hBrush);
+			EndPaint(hWnd, &ps);
+			break;
+			// cria copia em memoria
+			memDC = CreateCompatibleDC(hdc);
+			hBitmapDB = CreateCompatibleBitmap(hdc, rect.right, rect.bottom);
+			// aplicamos na copia em memoria as configs que obtemos com o CreateCompatibleBitmap
+			SelectObject(memDC, hBitmapDB);
+			DeleteObject(hBitmapDB);
+		}
+		// operações feitas na copia que é o memDC
+		FillRect(memDC, &rect, CreateSolidBrush(RGB(125, 125, 125)));
+
+		WaitForSingleObject(hMutex, INFINITE);
+		// operacoes de escrita da imagem - BitBlt
+		BitBlt(memDC, xBitmap, yBitmap, bmp.bmWidth, bmp.bmHeight, bmpDC, 0, 0, SRCCOPY);
+		ReleaseMutex(hMutex);
+
+		// bitblit da copia que esta em memoria para a janela principal - é a unica operação feita na janela principal
+		BitBlt(hdc, 0, 0, rect.right, rect.bottom, memDC, 0, 0, SRCCOPY);
+
+
+		// Encerra a pintura, que substitui o ReleaseDC
+		EndPaint(hWnd, &ps);
 		break;
-	case WM_DESTROY:	// Destruir a janela e terminar o programa 
-		// "PostQuitMessage(Exit Status)"		
+
+	case WM_ERASEBKGND:
+		return TRUE;
+
+		// redimensiona e calcula novamente o centro
+	case WM_SIZE:
+		WaitForSingleObject(hMutex, INFINITE);
+		xBitmap = (LOWORD(lParam) / 2) - (bmp.bmWidth / 2);
+		yBitmap = (HIWORD(lParam) / 2) - (bmp.bmHeight / 2);
+		limDir = LOWORD(lParam) - bmp.bmWidth;
+		memDC = NULL; // metemos novamente a NULL para que caso haja um resize na janela no WM_PAINT a janela em memoria é sempre atualizada com o tamanho novo
+		ReleaseMutex(hMutex);
+
+		break;
+
+	case WM_CLOSE:
+		// handle , texto da janela, titulo da janela, configurações da MessageBox(botoes e icons)
+		if (MessageBox(hWnd, TEXT("Tem a certeza que quer sair?"), TEXT("Confirmação"), MB_YESNO | MB_ICONQUESTION) == IDYES) {
+			// o utilizador disse que queria sair da aplicação
+			DestroyWindow(hWnd);
+		}
+		break;
+
+	case WM_DESTROY:	// Destruir a janela e terminar o programa
+		// "PostQuitMessage(Exit Status)"
 		PostQuitMessage(0);
 		break;
 	default:
