539886e4351548ec9d270313717dc86e317ec12c
MRodrigo1 <76001389+MRodrigo1@users.noreply.github.com>
2023-06-09 00:46:45 +0100

    dll changes

--- Frog/SharedMemory/SharedMemory.c
+++ Frog/SharedMemory/SharedMemory.c
@@ -3,8 +3,6 @@
 #include "../Frog/Utils.h"
 #include "../Frog/Struct.h"
 
-double factor = 1;
-
 HANDLE createMemoryMapping(){
 	return CreateFileMapping(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, 0, sizeof(GameData), FILE_MAPPING_GAME_DATA);
 }
