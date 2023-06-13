fd233910d2a652348c5e15ef06716abaface71cf
AngeloGSilva <47340682+AngeloGSilva@users.noreply.github.com>
2023-06-13 21:07:20 +0100

    delete old func

--- Frog/SharedMemory/SharedMemory.c
+++ Frog/SharedMemory/SharedMemory.c
@@ -1,23 +1,5 @@
 #include <windows.h>
 #include "SharedMemory.h"
-//#include "../Frog/Utils.h"
-//#include "../Frog/Struct.h"
-
-HANDLE createMemoryMapping(DWORD size,LPCSTR name){
-	return CreateFileMapping(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, 0, size, name);
-}
-
-HANDLE openMemoryMapping(DWORD dwDesiredAccess, LPCSTR lpName) {
-	return OpenFileMapping(dwDesiredAccess, FALSE ,lpName);
-}
-
-void copyMemoryOperation(PVOID destiny, VOID* source, SIZE_T Length) {
-	CopyMemory(destiny,source,Length);
-}
-
-void clearMemoryOperation(PVOID destination, SIZE_T Length) {
-	ZeroMemory(destination, Length);
-}
 
 TCHAR* InitSharedMemoryMapThreadRoads() {
 	HANDLE HMapFile = CreateFileMapping(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, 0, sizeof(TCHAR) * (MAX_ROWS + SKIP_BEGINING_END) * MAX_COLS, FILE_MAPPING_THREAD_ROADS);
--- Frog/SharedMemory/SharedMemory.h
+++ Frog/SharedMemory/SharedMemory.h
@@ -10,14 +10,6 @@
 #define DLL_IMP_API __declspec(dllimport)
 #endif
 
-DLL_IMP_API HANDLE createMemoryMapping(DWORD size, LPCSTR name);
-
-DLL_IMP_API HANDLE openMemoryMapping(DWORD dwDesiredAccess, LPCSTR lpName);
-
-DLL_IMP_API void copyMemoryOperation(PVOID destiny, VOID* source, SIZE_T Length);
-
-DLL_IMP_API void clearMemoryOperation(PVOID destination, SIZE_T Length);
-
 DLL_IMP_API TCHAR* InitSharedMemoryMapThreadRoads();
 DLL_IMP_API void SharedMemoryMapThreadRoads(pTRoads data);
 
